a0b867c92f3c5f1f9d237c899c4ed5d2c3a1f944
Eiryyy <xxx.eiryklav@gmail.com>
2018-07-25 17:36:15 +0900

    returns json

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const http = require('http')
+const url = require('url')
 http.createServer((req, res) => {
-  res.writeHead(200)
-  res.end()
+  const { query } = url.parse(req.url, true)
+  const json = {
+    counter: +query.c
+  }
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(json))
 }).listen(8081)
